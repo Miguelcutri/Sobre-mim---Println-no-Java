d899fd7d63104533bb003d63ca1ae4d3d529981c
Vitor <vitor.cutri@hotmail.com>
2020-11-12 15:30:00 -0300

    Quem sou eu (Em Java)

---
+++ ClasseHello.java
@@ -0,0 +1,15 @@
+package PacoteJava;
+
+public class ClasseHello {
+	public static void main(String args []) {
+		System.out.println("Olá mundo! Me chamo Vitor, mas pode me chamar de Vitinho,\n tenho 21 anos e estou cursando Análise "
+				+ "e desenvolvimento de sistemas\n e estou fazendo o Bootcamp da Generation, na qual,\n a experiência tem sido incrível!"
+				+ "\nSe me pedirem para escolher entre front, back ou banco de dados, eu escolheria\n o front-end, porque adoro "
+				+ " criar interfaces gráficas e lidar com a experiência\n do usuário."
+				+ "\nAlém do lado profissional, gosto muito de praticar esportes, skate, basquete, vôlei,\n tênis de mesa... Também "
+				+ "me divirto muito na tecnologia, jogos onlines sempre foi um\n dos meus maiores hobbies.\n Costumo frequentar a igreja "
+				+ "aos domingos, é o lugar onde eu mais me sinto bem,\n me denomino como Cristão praticante, e pretendo levar isso "
+				+ "para as pessoas ao meu redor.\n Meu maior sonho é construir uma família que venha influenciar e transformar as coisas\n"
+				+ "e as pessoas nessa geração.");
+	}
+}
